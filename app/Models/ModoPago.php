e9a1a286c101727eddd9f3cc99fe6ae7704bc98d
Julian Loaiza <julian.loaizal@udea.edu.co>
2022-04-11 16:21:09 -0500

    agregando crud

---
+++ app/Models/ModoPago.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModoPago extends Model
+{
+	use HasFactory;
+	
+    public $timestamps = true;
+
+    protected $table = 'modoPagos';
+
+    protected $fillable = ['id_pago','descripcion','otros_detalles','factura_id_factura','factura_cliente_id_vendedor','factura_pedido_id_pedido','factura_pedido_cliente_id_vendedor'];
+	
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function factura()
+    {
+        return $this->hasOne('App\Models\Factura', 'id_factura', 'factura_id_factura');
+    }
+    
+   
+    
+}
